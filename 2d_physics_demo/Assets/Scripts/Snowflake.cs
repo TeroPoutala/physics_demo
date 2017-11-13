992703c2e030297c37d1dd1cd7b88143d61673c8
Tero Poutala <teropoutala@kamk.fi>
2017-11-13 14:36:36 +0200

    Added things into Scene1.

---
+++ 2d_physics_demo/Assets/Scripts/Snowflake.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowflake : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.gameObject.transform.position += Vector3.down * (5f * Time.deltaTime);
+	}
+}
---
+++ 2d_physics_demo/Assets/Scripts/SpawnSnow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSnow : MonoBehaviour {
+
+    public GameObject snowFlake;
+
+    private float spawnerLenght;
+
+	// Use this for initialization
+	void Start ()
+    {
+        spawnerLenght = this.gameObject.transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        SpawnSnowflake();
+	}
+
+    void SpawnSnowflake()
+    {
+        float snowflakePosX = UnityEngine.Random.Range(0, spawnerLenght);
+        Vector3 snowflakePos = new Vector3(this.gameObject.transform.localScale.x / 2 - snowflakePosX, this.gameObject.transform.position.y);
+        Instantiate(snowFlake, snowflakePos, this.gameObject.transform.rotation);
+    }
+}

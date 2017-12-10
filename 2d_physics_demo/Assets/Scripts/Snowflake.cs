df90744b4c9a9b0e33811c9c31083cd3eb620428
Tero Poutala <teropoutala@kamk.fi>
2017-12-10 18:10:44 +0200

    Project is ready

--- 2d_physics_demo/Assets/Scripts/Snowflake.cs
+++ 2d_physics_demo/Assets/Scripts/Snowflake.cs
@@ -6,16 +6,27 @@ public class Snowflake : MonoBehaviour {
 
     private float scale;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         scale = Random.Range(0.1f, 0.4f);
         this.gameObject.transform.localScale = new Vector3(scale, scale);
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         this.gameObject.transform.position += Vector3.down * ((scale + 1f) * Time.deltaTime);
-	}
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        float gotWindSpeed;
+
+        if (collision.gameObject.tag == "WindTrigger")
+        {
+            gotWindSpeed = collision.gameObject.GetComponent<WindTrigger>().windSpeed;
+            this.gameObject.transform.position += Vector3.right * (scale * gotWindSpeed) * Time.deltaTime;
+        }
+    }
 }
